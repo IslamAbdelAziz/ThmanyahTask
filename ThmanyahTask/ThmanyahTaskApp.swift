64a7dc0792d9b1145d1a87fcd4d8376c47ab4c26
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2025-08-08 15:22:39 +0300

    Initial Commit

---
+++ ThmanyahTask/ThmanyahTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahTaskApp.swift
+//  ThmanyahTask
+//
+//  Created by iSlam AbdelAziz on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
